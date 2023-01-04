5537f4e21bb811fb0695f4a13bf6df53e0875303
NguyenHoang1303 <nguyenhsth2011012@fpt.edu.vn>
2023-01-04 10:24:37 +0700

    ghep api home page

--- src/app/shared/utils/SystemUtil.ts
+++ src/app/shared/utils/SystemUtil.ts
@@ -1,5 +1,6 @@
 export class SystemUtil{
 
-  public static BASE_URL = 'http://localhost:8080'
+  public static BASE_URL = 'https://herofund.up.railway.app';
+  static local = 'http://localhost:8080';
 
 }
