540a62e018608e189d21564a08192f1180d9e2f5
NguyenHoang1303 <nguyenhsth2011012@fpt.edu.vn>
2023-01-16 09:59:44 +0700

    ghep api home page

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { catchError, Subject, tap, throwError } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { SystemUtil } from "../utils/SystemUtil";
+import {Campaign} from "../entity/Modal";
 
 @Injectable({
   providedIn: 'root'
@@ -23,65 +24,25 @@ export class ApiService {
     let query = `/api/v1/campaigns?offset=${offset}&limit=${limit}`;
     return this.http
       .get<any>(SystemUtil.BASE_URL + query)
-      .pipe(
-        catchError((error: any) => {
-          return throwError(error);
-        }),
-        tap(() => {
-          this.RefreshData.next();
-        })
-      );
   }
   public getCampaigns() {
     let query = `/api/v1/campaigns/all`;
     return this.http
       .get<any>(SystemUtil.BASE_URL + query)
-      .pipe(
-        catchError((error: any) => {
-          return throwError(error);
-        }),
-        tap(() => {
-          this.RefreshData.next();
-        })
-      );
   }
 
   public getSponsor() {
     return this.http
       .get<any>(SystemUtil.BASE_URL + '/api/v1/sponsors')
-      .pipe(
-        catchError((error: any) => {
-          return throwError(error);
-        }),
-        tap(() => {
-          this.RefreshData.next();
-        })
-      );
   }
 
   public getRandomCategories(random : number) {
     return this.http
       .get<any>(SystemUtil.BASE_URL + `/api/v1/categories/random?number=${random}`)
-      .pipe(
-        catchError((error: any) => {
-          return throwError(error);
-        }),
-        tap(() => {
-          this.RefreshData.next();
-        })
-      );
   }
 
   public getCampaignUrgent() {
     return this.http
-      .get<any>(SystemUtil.BASE_URL + `/api/v1/campaigns/urgent`)
-      .pipe(
-        catchError((error: any) => {
-          return throwError(error);
-        }),
-        tap(() => {
-          this.RefreshData.next();
-        })
-      );
+      .get<Campaign>(SystemUtil.BASE_URL + `/api/v1/campaigns/urgent/random`)
   }
 }
